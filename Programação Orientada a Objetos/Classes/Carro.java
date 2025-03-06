315f9c00b045217568b9951cfda8dd2f714072b9
Victor2412 <victor.git24@gmail.com>
2025-03-06 06:48:37 -0300

    Classes

---
+++ Programação Orientada a Objetos/Classes/Carro.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Carro {
+
+    String marca ;
+    String modelo ;
+    int ano ;
+
+    void exibirInformações(){
+        System.out.println("Marca : " + marca);
+        System.out.println("Modelo : " + modelo);
+        System.out.println("Ano : " + ano);
+    }
+
+
+
+}

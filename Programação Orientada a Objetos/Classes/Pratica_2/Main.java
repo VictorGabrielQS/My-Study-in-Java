c23cab9931cf52b3a7380a057b6f07b638332642
Victor2412 <victor.git24@gmail.com>
2025-03-07 07:12:45 -0300

    Exercicios Objeto

---
+++ Programação Orientada a Objetos/Classes/Pratica_2/Main.java
@@ -0,0 +1,14 @@
+package Classes.Pratica_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+
+        carro1.setMarca("BMW");
+        carro1.setModelo("320i");
+        carro1.setAno(2024);
+
+        carro1.imprimirDetalhes();
+    }
+
+}

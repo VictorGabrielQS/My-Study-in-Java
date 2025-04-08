ad438cc31a38b8499c4a085fb8e48765960b6270
Victor2412 <victor.git24@gmail.com>
2025-04-08 06:19:08 -0300

    Stream start

---  Sintaxe Avançada/Streams_API_filter_map_reduce/Stream/Main.java
+++  Sintaxe Avançada/Streams_API_filter_map_reduce/Stream/Main.java
@@ -6,11 +6,25 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
+        //Criado uma Lista -
         List<String> nomes = List.of("Ana", "João", "Maria", "Pedro");
 
+        //Criando um Stream e fazendo ele receber os valores da lista recente criada
         Stream<String> stream = nomes.stream();
 
-        System.out.println(stream);
+        //filter(Predicate<T>) - Filtra elementos com base em uma condição.
+        //Exemplo: Filtrar nomes que começam com "J"
+
+        System.out.println(stream.filter(
+                nome -> nome.startsWith("J"))
+                .toList() // Lista o item encontrado que começa com a letra "J"
+        );
+
+
+        // map(Function<T, R>) -
+
+
+
 
     }
 }
